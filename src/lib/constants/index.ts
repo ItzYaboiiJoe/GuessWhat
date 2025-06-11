13e62ca3a2d483d2ef90adc1b674f1daf4f72f02
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-06-10 22:53:22 -0400

    Initial commit

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = "Guess Where";
+export const APP_DESCRIPTION =
+  "A simple game app to guess what location is shown in the image";
+export const SERVER_URL = "http://localhost:3000";

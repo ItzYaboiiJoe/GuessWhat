84127f6595fb09093788a1bc516f0c20caf9dcd9
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-06-11 18:29:05 -0400

    Created a new file that will host all the available options and to be randomized on the options for selecting the answer

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,8 @@
 import fetchImage from "@/components/api call";
+import { options } from "@/components/options";
+
+const getRandomOption = () =>
+  options[Math.floor(Math.random() * options.length)];
 
 const Home = async () => {
   const apod = await fetchImage();
@@ -6,13 +10,18 @@ const Home = async () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="flex flex-col space-y-4 items-center mb-50">
-        <h1 className="text-2xl mb-10">What is this?</h1>
+        <h1 className="text-4xl font-bold text-gray-900 leading-tight mb-2 pb-4 relative mb-5">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">
+            What is this?
+          </span>
+          <span className="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-purple-500 to-pink-500"></span>
+        </h1>
         <img src={apod.url} alt={apod.title} className="w-lg" />
-        <ul className="flex items-center flex-col space-y-4 mt-10">
-          <li>Option A</li>
+        <ul className="flex items-center flex-col space-y-4 mt-10 p-3 bg-red-500 rounded-md">
+          <li>{getRandomOption()}</li>
           <li>{apod.title}</li>
-          <li>Option C</li>
-          <li>Option D</li>
+          <li>{getRandomOption()}</li>
+          <li>{getRandomOption()}</li>
         </ul>
       </div>
     </div>
---
+++ src/components/options/index.ts
@@ -0,0 +1,12 @@
+export const options = [
+  "The Milky Way",
+  "Andromeda Galaxy",
+  "Horsehead Nebula",
+  "Orion Nebula",
+  "Crab Nebula",
+  "Saturn's Rings",
+  "Jupiter's Great Red Spot",
+  "The Moon",
+  "Mars Surface",
+  "Venus Atmosphere",
+];

4d09510f101f39859b68b871aca72e5417c51a5f
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-06-11 18:44:39 -0400

    Created Randomized System for options positioning and added comments in case Elijah wants to read

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,24 @@
 import fetchImage from "@/components/api call";
 import { options } from "@/components/options";
 
+// Function to get a random option from the options array
 const getRandomOption = () =>
   options[Math.floor(Math.random() * options.length)];
 
 const Home = async () => {
+  // Fetch the Astronomy Picture of the Day (APOD) data
   const apod = await fetchImage();
 
+  // Will create a randomized list of options with the correct answer and 3 incorrect answers
+  const optionsList = [
+    apod.title, // Correct answer
+    getRandomOption(), // Random incorrect answers
+    getRandomOption(), // Random incorrect answers
+    getRandomOption(), // Random incorrect answers
+  ];
+  optionsList.sort(() => Math.random() - 0.5);
+
+  // UI
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="flex flex-col space-y-4 items-center mb-50">
@@ -17,11 +29,11 @@ const Home = async () => {
           <span className="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-purple-500 to-pink-500"></span>
         </h1>
         <img src={apod.url} alt={apod.title} className="w-lg" />
+        {/* The Red Background is a temp so I know the paddings and the size of the options when creating buttons */}
         <ul className="flex items-center flex-col space-y-4 mt-10 p-3 bg-red-500 rounded-md">
-          <li>{getRandomOption()}</li>
-          <li>{apod.title}</li>
-          <li>{getRandomOption()}</li>
-          <li>{getRandomOption()}</li>
+          {optionsList.map((option, idx) => (
+            <li key={idx}>{option}</li>
+          ))}
         </ul>
       </div>
     </div>

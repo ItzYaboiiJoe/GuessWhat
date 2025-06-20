1793cc3a1336641f9b0ceffb73fbe8722ed341f4
Joseph Seoudy <josephakseoudy@gmail.com>
2025-06-20 08:24:03 -0400

    Created a Footer and an empty about page

--- src/app/(root)/about/layout.tsx
+++ src/app/(root)/about/layout.tsx
@@ -1,6 +1,3 @@
-import Header from "@/components/shared/header";
-import Footer from "@/components/shared/footer";
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,9 +5,7 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-screen flex-col">
-      <Header />
       <div className="bg-[#F8ECE1]">{children}</div>
-      <Footer />
     </div>
   );
 }
---
+++ src/app/(root)/about/page.tsx
@@ -0,0 +1,7 @@
+const About = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen px-4"></div>
+  );
+};
+
+export default About;

5c57fe91084b51c4e560773be9b772a1ccc77deb
Joseph Seoudy <josephakseoudy@gmail.com>
2025-06-23 15:06:06 -0400

    Styled the not found page and deleted loading page since its not needed at the moment

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,16 @@ import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+      {/* Background Layer */}
+      <div
+        className="absolute inset-0 -z-10 bg-cover bg-center"
+        style={{
+          backgroundImage: "url('/images/Space.svg')",
+        }}
+      />
+
+      {/* Content */}
       <Image
         src={"/images/QuestionMark.svg"}
         width={48}
@@ -14,12 +23,14 @@ const NotFoundPage = () => {
         alt={`${APP_NAME} logo`}
         priority={true}
       />
-      <div className="p-6 rounded-lg shadow-md text-center">
-        <h1 className="text-3xl font-bold mb-4">Not Found</h1>
-        <p className="text-destructive">Could not find requested page</p>
+      <div className="p-6 rounded-lg shadow-md text-center backdrop-blur-sm bg-black/40">
+        <h1 className="text-3xl font-bold mb-4 text-white">Not Found</h1>
+        <p className="text-pink-400">Could not find requested page</p>
         <Link href={"/"} passHref>
-          <Button variant={"outline"} className="mt-4 ml-2 cursor-pointer">
-            {" "}
+          <Button
+            variant={"outline"}
+            className="mt-4 ml-2 hover:opacity-70 cursor-pointer"
+          >
             Back To Home
           </Button>
         </Link>

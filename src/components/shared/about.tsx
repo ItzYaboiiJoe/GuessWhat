4b2374c72973d19223c9392ca8ee13b66e382cf7
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-06-26 21:42:14 -0400

    Resolved a bug that made the about button not work properly

--- src/components/shared/about.tsx
+++ src/components/shared/about.tsx
@@ -4,7 +4,10 @@ import Link from "next/link";
 const AboutButton = () => {
   return (
     <div className="flex justify-end gap-3">
-      <Button className="bg-gradient-to-r from-purple-500 to-pink-500 text-white font-semibold py-2 rounded-md hover:opacity-90 transition-all cursor-pointer">
+      <Button
+        asChild
+        className="bg-gradient-to-r from-purple-500 to-pink-500 text-white font-semibold py-2 rounded-md hover:opacity-90 transition-all cursor-pointer"
+      >
         <Link href={"/about"}>About</Link>
       </Button>
     </div>

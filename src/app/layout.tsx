ea712d04b46c8baf8e94ee2d550a43cd92a1f066
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-06-26 21:26:13 -0400

    Implemented Speed and Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ import {
   KeyWords,
   Graph,
 } from "@/lib/constants/index";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,7 +41,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }

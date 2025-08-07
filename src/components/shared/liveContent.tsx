2be1f3fcdb2f70f4989ad906c39e8d60ffe75f0a
Joseph Seoudy <josephakseoudy@gmail.com>
2025-08-07 13:13:58 -0400

    Changed Button variant  from link to defaults

--- src/components/shared/liveContent.tsx
+++ src/components/shared/liveContent.tsx
@@ -130,7 +130,7 @@ export default function LiveContent() {
               transition={{ delay: 1.3, duration: 0.6, ease: "easeOut" }}
             >
               <Button
-                variant="link"
+                variant="default"
                 className="relative bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500 hover:cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:origin-left after:bg-gradient-to-r after:from-purple-500 after:to-pink-500"
                 onClick={() => setShowArchive(true)}
               >
